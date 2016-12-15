cb8b18bfb40e1da6592ff62e8c19b9bc12e6b7ee
Xinyuan Wang <xwang146@binghamton.edu>
2016-12-14 21:06:43 -0500

    found way to link prediction page and process page, could let user select predictors and get the value, still working on the location predictor

--- Website_for_prediction/js/main.js
+++ Website_for_prediction/js/main.js
@@ -1,2 +1,21 @@
 //get user predictors from index page
-//run the model and print the result in the prediction page
+//run the model and print the result in the prediction p
+
+function predict(){
+    // got the user input in every field
+
+    var property_type = $('#property_type input:radio:checked').val();
+    var room_type = $('#room_type input:radio:checked').val();
+    var bed_type = $('#bed_type input:radio:checked').val();
+    console.log(property_type);
+    console.log(room_type);
+    console.log(bed_type);
+
+    var accommodate = document.getElementById('accommodates').value;
+    console.log("here");
+    console.log(accommodate);
+
+
+
+
+}
